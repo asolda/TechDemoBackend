8eda37758dedab77ae70151aeab1092cf503f879
asolda <a.solda@studenti.unisa.it>
2016-12-01 09:12:31 +0100

    Commento

--- routes.js
+++ routes.js
@@ -1,26 +1,27 @@
 var user = require('./models/user');
- 
-module.exports = {
-  configure: function(app) {
-    
-    app.get('/user/', function(req, res) {
-      user.get(res);
-    });
- 
-    app.post('/user/', function(req, res) {
-      user.create(req.body, res);
-    });
- 
-    app.put('/user/', function(req, res) {
-      user.update(req.body, res);
-    });
- 
-    app.delete('/user/:email/', function(req, res) {
-      user.delete(req.params.email, res);
-    });
 
-    app.get('/user/search/:email/', function(req, res) {
-      user.search(req.params.email, res);
-    });
-  }
+// API esportate e disponibili al frontend
+module.exports = {
+    configure: function(app) {
+
+        app.get('/user/', function(req, res) {
+            user.get(res);
+        });
+
+        app.post('/user/', function(req, res) {
+            user.create(req.body, res);
+        });
+
+        app.put('/user/', function(req, res) {
+            user.update(req.body, res);
+        });
+
+        app.delete('/user/:email/', function(req, res) {
+            user.delete(req.params.email, res);
+        });
+
+        app.get('/user/search/:email/', function(req, res) {
+            user.search(req.params.email, res);
+        });
+    }
 };
